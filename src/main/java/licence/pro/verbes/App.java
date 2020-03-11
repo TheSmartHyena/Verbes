4df616314f1468a13d73e3aff293aa18565bacd7
SkopalPhilippe <skopal.philippe@gmail.com>
2020-03-11 15:57:54 +0100

    1.0
    Initial commit

--- src/main/java/licence/pro/verbes/App.java
+++ src/main/java/licence/pro/verbes/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Ici seront conjugué les verbes" );
-        System.out.println( "Ici seront conjugué les verbes" );
+       
     }
 }

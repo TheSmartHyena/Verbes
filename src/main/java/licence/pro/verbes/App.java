88cd447d8cf319ef781e62ff71119244d5170c3c
SkopalPhilippe <skopal.philippe@gmail.com>
2020-03-11 15:57:31 +0100

    Initial commit

---
+++ src/main/java/licence/pro/verbes/App.java
@@ -0,0 +1,14 @@
+package licence.pro.verbes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Ici seront conjugué les verbes" );
+        System.out.println( "Ici seront conjugué les verbes" );
+    }
+}
